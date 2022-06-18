c1eda3d98b2add476b9571c7316d4a52d5d182f0
volkan <volkandogru11@gmail.com>
2022-06-18 09:39:57 +0300

    dom form ile toplama işlemi yapıldı

--- form/form.js
+++ form/form.js
@@ -1,10 +1,14 @@
-let anaForm = document.querySelector("#userForm");
-anaForm.addEventListener("submit", formSubmit);
 
 
-function formSubmit(e) {
+var btn = document.querySelector("#btn");
+
+btn.addEventListener("click" , hesapla);
+
+function hesapla(e){
     e.preventDefault();
-    console.log("işlem gerçekleşti");
-    let score = document.querySelector("#score");
-    console.log(score.value);
+    var sayi1 = document.querySelector("#sayi1").value;
+    var sayi2 = document.querySelector("#sayi2").value;
+    var sonuc = document.querySelector("#sonuc");
+    var sonuc =parseInt(sayi1) + parseInt(sayi2);
+    document.querySelector("#sonuc").value = sonuc;
 }
