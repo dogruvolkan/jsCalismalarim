842de173ca8bb9405724fb2ac4022ec61c79609b
volkan <volkandogru11@gmail.com>
2022-06-21 16:05:28 +0300

    to do yapildi

--- toDo/script.js
+++ toDo/script.js
@@ -1,48 +1,45 @@
-const inputArea = document.querySelector("#inputValue");
-const btnEkle = document.querySelector("#btnEkle");
-const btnSil = document.querySelector("#btnSil");
-const listDom = document.querySelector("#listItems");
+let inputDom = document.querySelector("#task");
+let btnAddDom = document.querySelector("#liveToastBtn");
+let listDom = document.querySelector("#list");
+
+btnAddDom.addEventListener("click", addItem);
 
 
+function addItem() {
+    if (inputDom.value) {
+        let li = document.createElement("li");
+        li.innerHTML = inputDom.value;
+        listDom.appendChild(li);
+        var removeBtn = document.createElement("span");
+        removeBtn.classList.add("close");
+        removeBtn.innerHTML = "X";
+        li.appendChild(removeBtn);
+        inputDom.value = "";
+        $(".success").toast("show");
+        removeBtn.addEventListener("click", removeItem);
 
-btnEkle.addEventListener("click", addItem);
-btnSil.addEventListener("click", clearAllItem);
+        function removeItem() {
+            var answer = window.confirm("kaldırmak istediğinizden emin misiniz?");
+            if (answer) {
+                li.remove();
+                $(".remove").toast("show");
+            }
+        }
 
+        li.addEventListener("click", selectItem);
+        function selectItem(){
+            li.classList.toggle("checked");
+        }
 
+        const removeAllDom = document.querySelector("#btnRemoveAll");
+        removeAllDom.addEventListener("click", removeAll);
 
-var option = {
-    animation: true,
-    delay: 2000,
-};
+        function removeAll() {  
+                listDom.remove();
+            
+        }
 
-
-function toast() {
-    var toastDom = document.querySelector("#toasts");
-    toastDom.innerHTML = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-  </div>`;
-    var toastElement = new bootstrap.Toast(toastDom, option);
-    toastElement.show();
-    console.log("eklendi");
-}
-
-function addItem(e) {
-    e.preventDefault();
-    const listItem = document.createElement("li");
-    listItem.classList.add("list_item");
-    listItem.innerHTML = inputArea.value;
-    listDom.appendChild(listItem);
-    toast();
-    inputArea.value = "";
-}
-
-function clearAllItem() {
-    const listItem = document.querySelector("ul");
-    listItem.remove();
-    
+    } else {
+        $(".error").toast("show");
+    }
 }
